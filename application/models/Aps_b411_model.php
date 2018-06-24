55b218a623b1fb9e6d3ffd0dfc638c71cd3a341b
Teguh Prabowo <katateguh345@gmail.com>
2018-06-24 14:41:19 +0700

    add

--- application/models/Aps_b411_model.php
+++ application/models/Aps_b411_model.php
@@ -10,11 +10,28 @@ public function __construct()
  }
 
 // Listing
- public function listing() {
- $this->db->select('*');
- $this->db->from('aps_b411');
- $query = $this->db->get();
- return $query->result();
+ public function GetProdi() {
+ $prodi=$this->db->query('SELECT CONCAT(p.prodi," ",p.jurusan) AS prodi FROM sdm s INNER JOIN prodi_tbl p ON s.kd_prodi=p.kode_prodi;');
+ return $prodi->result_array();
  }
 
+ public function listing1() {
+ $data1=$this->db->query('SELECT total,jml FROM sdm WHERE kd_hal=1 AND kd_prodi="p001"');
+ return $data1->result_array();
+ }
+
+ public function listing2() {
+ $data2=$this->db->query('SELECT total,jml FROM sdm WHERE kd_hal=2 AND kd_prodi="p001"');
+ return $data2->result_array();
+ }
+
+public function listing3() {
+ $data3=$this->db->query('SELECT total,jml FROM sdm WHERE kd_hal=3 AND kd_prodi="p001"');
+ return $data3->result_array();
+ }
+
+ public function listing4() {
+ $data4=$this->db->query('SELECT total,jml FROM sdm WHERE kd_hal=4 AND kd_prodi="p001"');
+ return $data4->result_array();
+ }
 }
--- application/views/User/Butir4B/tampilan_borang4.1.1.php
+++ application/views/User/Butir4B/tampilan_borang4.1.1.php
@@ -106,22 +106,6 @@ License: You must have a valid license purchased only from themeforest(the above
 											</a> -->
 										</div>
 									</div>
-									<!-- <div class="col-md-6">
-										<div class="btn-group pull-right">
-											<button class="btn dropdown-toggle" data-toggle="dropdown">Tools <i class="fa fa-angle-down"></i>
-											</button>
-											<ul class="dropdown-menu pull-right">
-												<li>
-													<a href="javascript:;">
-													Print </a>
-												</li>
-												<li>
-													<a href="javascript:;">
-													Export to Excel </a>
-												</li>
-											</ul>
-										</div>
-									</div> -->
 								</div>
 							</div>
 							<div style="overflow-x:auto;">
@@ -134,13 +118,16 @@ License: You must have a valid license purchased only from themeforest(the above
 									 <th colspan="7">Jumlah Dosen yang bertugas pada PS: *</th>
 								 </tr>
 								 <tr>
-								 	<th>PS-1 <br>Nama PS-1</th>
+								 <?php foreach ($prodi as $p) {
+								  ?>
+								 	<th>PS-1 <br><?php echo $p['prodi']; ?></th>
 								 	<th>PS-2 <br>Nama PS-2</th>
 								 	<th>PS-3 <br>Nama PS-3</th>
 								 	<th>PS-4 <br>Nama PS-4</th>
 								 	<th></th>
 								 	<th></th>
 								 	<th></th>
+								 <?php } ?>
 								 </tr>
 								 <tr align="center">
 								 	<td>(1)</td>
@@ -156,157 +143,164 @@ License: You must have a valid license purchased only from themeforest(the above
 								 </tr>
 							</thead>
 							<tbody>
-							<!-- <?php 
-								$no=1; 
-								foreach ($data as $d ) { 
-							?> -->
-							<!-- <tr class="odd gradeX">
-								<td>
-									<?php echo $no++; ?>
-								</td>
-								<td>
-									 <?php echo $d['kode_fakultas']; ?>
-								</td>
-								<td>
-									<?php echo $d['nama_fakultas']; ?>
-								</td>
-								<td class="center">
-									<a href="<?php echo base_url()."index.php/CrudFakultas/edit_data/".$d['kode_fakultas'];?>">Edit</a> ||
-         							<a href="<?php echo base_url()."index.php/CrudFakultas/do_hapus/".$d['kode_fakultas'];?>">Delete</a>
-								</td>
-							</tr>
-							<?php } ?> -->
 							<tr>
 							 	<th>A</th>
 							 	<th>Jabatan Fungsional :</th>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td>1</td>
 							 	<td>Asisten Ahli</td>
-							 	<td>5</td>
-							 	<td>5</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data1 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td>2</td>
 							 	<td>Lektor</td>
-							 	<td>1</td>
-							 	<td>1</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data2 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td>3</td>
 							 	<td>Lektor Kepala</td>
-							 	<td>0</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data3 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							  <tr>
 							 	<td>4</td>
 							 	<td>Guru Besar/Profesor</td>
-							 	<td>0</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data4 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td></td>
 							 	<td>Total</td>
-							 	<td>6</td>
-							 	<td>6</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
+							 	<?php 
+							 		foreach ($total1 as $t) {
+							 	 ?>
+							 	 <td><?php echo $t['totala']; ?></td>
+							 	 <td><?php echo $t['totalaps1']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<th>B</th>
 							 	<th>Pendidikan Tertinggi:</th>
 							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							  <tr>
 							 	<td>1</td>
 							 	<td>S1</td>
-							 	<td>1</td>
-							 	<td>1</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data5 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td>2</td>
 							 	<td>S2/Profesi/Sp-1</td>
-							 	<td>7</td>
-							 	<td>7</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data6 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td>3</td>
 							 	<td>S3/Sp-2</td>
-							 	<td>0</td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
-							 	<td></td>
+							 	<?php 
+							 		foreach ($data7 as $d) {
+							 	 ?>
+							 	 <td><?php echo $d['total']; ?></td>
+							 	 <td><?php echo $d['jml']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							 <tr>
 							 	<td></td>
 							 	<td>Total</td>
-							 	<td>8</td>
-							 	<td>8</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
-							 	<td>0</td>
+							 	<?php 
+							 		foreach ($total2 as $t) {
+							 	 ?>
+							 	 <td><?php echo $t['totalb']; ?></td>
+							 	 <td><?php echo $t['totalbps1']; ?></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
+							 	 <td></td>
 							 </tr>
 							</tbody>
 							</table>
